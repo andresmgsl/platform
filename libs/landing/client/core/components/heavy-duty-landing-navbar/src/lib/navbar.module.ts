a97d46427c48b4a81d9c7833dc8f64a562c197fc
Andres Guerrero <andresmgsl@gmail.com>
2022-10-24 00:59:34 +0100

    style: added more style to terms and condition component

--- libs/landing/client/core/components/heavy-duty-landing-navbar/src/lib/navbar.module.ts
+++ libs/landing/client/core/components/heavy-duty-landing-navbar/src/lib/navbar.module.ts
@@ -1,9 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { HeavyDutyLandingNavbarComponent } from './navbar.component';
 
 @NgModule({
-	imports: [CommonModule],
+	imports: [CommonModule, RouterModule],
 	declarations: [HeavyDutyLandingNavbarComponent],
 	exports: [HeavyDutyLandingNavbarComponent],
 })
